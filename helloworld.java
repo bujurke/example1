dc476aa6d2a81ac75ca47a6640a7aa4e71c3a53f
bujurke <ambikabujurke22@gmail.com>
2021-01-25 13:24:45 +0530

    first java program

---
+++ helloworld.java
@@ -0,0 +1,8 @@
+public class Helloworld {
+    
+     public static void main (string[] args) {
+         //prints "Hello, World" to the terminal window.
+         system.out.println("Hello, World") ;
+     }
+     
+}         
